fbe60e8e003fb3872d171d53e32dde6efecb335e
Neha Bhaskar <bhaskar.neha23@gmail.com>
2025-04-19 12:05:03 +0530

    Init spring+mysql setup

---
+++ nibblenotebook/src/main/java/com/example/nibblenotebook/NibblenotebookApplication.java
@@ -0,0 +1,13 @@
+package com.example.nibblenotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NibblenotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NibblenotebookApplication.class, args);
+	}
+
+}
---
+++ nibblenotebook/src/test/java/com/example/nibblenotebook/NibblenotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nibblenotebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NibblenotebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
